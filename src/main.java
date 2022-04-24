343800cd3adbdd3ed97ea6015116d618a0c0eae6
filippobartolucci <filippo.bartolucci2@studio.unibo.it>
2022-04-25 00:46:53 +0200

    pulizia codice dopo prima consegna

--- src/main.java
+++ src/main.java
@@ -14,11 +14,10 @@ import java.util.ArrayList;
 import Semantic.*;
 
 enum exitCode{
-    LEXER_ERROR,
+    SUCCESS,
     PARSER_ERROR,
     SEMANTIC_ERROR,
     RUNTIME_ERROR,
-    SUCCESS
 }
 
 public class main {
@@ -36,46 +35,22 @@ public class main {
             System.out.println("File: \"" + file + "\" found.\n\nParsing...");
 
             AssetLanLexer lexer = new AssetLanLexer(CharStreams.fromFileName(file));
-
-            // Our LexerErrorListener, used later to dump errors to txt
-            LexerErrorListener lexerListener = new LexerErrorListener();
-            lexer.addErrorListener(lexerListener);
-
             CommonTokenStream cts = new CommonTokenStream(lexer);
             AssetLanParser parser = new AssetLanParser(cts);
 
             AssetLanVisitorImpl visitor = new AssetLanVisitorImpl();
             Node ast = visitor.visitInit(parser.init());
 
-            // Ex1
-            PrintWriter out = null;
-            if (lexerListener.getLexerErrors().size()>0){
-                try {
-                    out = new PrintWriter("lexer_errors.txt");
-                    for (Error error : lexerListener.getLexerErrors()) {
-                        out.write(error.getErrorMessage()+"\n");
-                    }
-                    out.flush(); // Flush the output, save the file
-                } catch (FileNotFoundException ex) {ex.printStackTrace();}
-            }
-            if (out != null) {
-                out.close();
-                System.exit(exitCode.LEXER_ERROR.ordinal());
-            }
-
-
             if (parser.getNumberOfSyntaxErrors()>0) {
                 System.err.println("\n" + parser.getNumberOfSyntaxErrors() + " Syntax errors found -> Compilation failed.");
                 // PARSER ERROR EXIT CODE
-                return;
+                System.exit(exitCode.PARSER_ERROR.ordinal());
             }
 
             System.out.println("Parsing successful!\n\nSemantic analysis...");
 
 
-            // Ex2
             Environment env = new Environment();
-
             ArrayList<SemanticError> s_errors;
             s_errors = ast.checkSemantics(env);
 
@@ -84,13 +59,15 @@ public class main {
                     System.err.println(s_error.toString());
                 }
                 System.err.println("\n" + s_errors.size() + " Semantic errors found -> Compilation failed.");
-                System.exit(-1);
+                System.exit(exitCode.SEMANTIC_ERROR.ordinal());
             }
-            System.out.println("Semantic analysis successful!\n");
+            System.out.println("Semantic analysis successful!");
+
+            System.exit(exitCode.SUCCESS.ordinal());
 
         }catch (Exception exc) {
             System.err.println(exc.getMessage());
-            System.exit(2);
+            System.exit(exitCode.RUNTIME_ERROR.ordinal());
         }
     }
 
